4acb0d9a2b1bf61cf7db51f9bc4a6e76c581934b
Leo Alekseev <leo@starlingminds.com>
2017-02-20 11:36:53 -0800

    STAR-1000: fixed tests

--- tests/unit/pods/application/route-test.js
+++ tests/unit/pods/application/route-test.js
@@ -1,11 +1,13 @@
 import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:application', 'Unit | Route | application', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+    // Specify the other units that are required for this test.
+    needs: ['service:session']
 });
 
 test('it exists', function(assert) {
-  let route = this.subject();
-  assert.ok(route);
+    'use strict';
+
+    let route = this.subject();
+    assert.ok(route);
 });
